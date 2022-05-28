e67714ae0ec21e6d0374c1363a0be19c119b870e
Pedro.sandrini <pedro.sandri@markdigx.com.br>
2022-05-27 21:23:23 -0300

    [Pedro] Aula 03 - identificando tipos de variaveis

---
+++ aula03.js
@@ -0,0 +1,7 @@
+// como identificar uma variavel e o tipo que ela tem? 
+// let nome = "Pedro Henrique";
+// let idade = 25;
+// let QuantidadePets = 4;
+// let porta = true;
+// let money ;
+// console.log(typeof(porta))
