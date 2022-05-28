0e82dd94f427a6308488d26f7223eddf4caf5438
Pedro.sandrini <pedro.sandri@markdigx.com.br>
2022-05-27 21:04:15 -0300

    [Pedro] Aula 01 - Variaveis

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # aulao
 Praticando Js, cada commit é uma aula
+
+01- Variavel (let,var,const)
+
---
+++ index.js
@@ -0,0 +1,10 @@
+/* let nome = 'Pedro';
+let idade = 25;
+let cidade = 'Londrina'
+console.log('Olá meu nome é ' + nome + ' e minha idade é ' + idade + ' anos e  atualmente moro em ' + cidade);
+*/
+
+// reatribuindo valores e explicar constante
+// let carteira = 10;
+// carteira = 50;
+//  console.log(carteira);
