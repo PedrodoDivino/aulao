26a2dc49dafdfd26816af1a7594dcd235e5419a3
Pedro.sandrini <pedro.sandri@markdigx.com.br>
2022-05-27 23:30:46 -0300

    [Pedro] aula 07 - funções - tarefa e retorno (return)

---
+++ aula07.js
@@ -0,0 +1,14 @@
+//fazer uma tarefa com função 
+
+function ola () {
+    console.log('fala pessoal, espero que esteja todo mundo bem')
+}
+
+// adicionar e multiplicar com função 
+function multi(v) {
+    return v * 10
+    
+}
+// console.log(multi(5))
+let result = multi(2)
+console.log(result)
