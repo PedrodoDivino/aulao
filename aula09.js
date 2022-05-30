f45854758e54fe22773ff23e0ef29e70c2aee9d7
Pedro.sandrini <pedro.sandri@markdigx.com.br>
2022-05-30 09:39:16 -0300

    [Pedro] Aula 09 P2 - operadores de igualdade

--- aula09.js
+++ aula09.js
@@ -1,5 +1,23 @@
-let tenis = 100;
+// let tenis = 100;
 // atribuição é a mesma coisa que tenis = tenis + tenis,  só que mais rápido
-tenis += tenis;
+// tenis += tenis;
 
-console.log(tenis)
+// console.log(tenis)
+
+// operadores de igualdade
+// igualdade estrita 
+
+console.log( 1 === 1);
+//vai retorar true, ele está dizendo que é identico
+console.log("1" === 1)
+//vai retornar false pq o um ali, e'um string, e o ouro é number
+console.log( 1 == 1 );
+//vai retornar true pq 1 é igual a 1
+console.log("1" == 1);
+//aqui ele vai retorar true pq, ele ignora a string e e utiliza somente o numero
+// não é recomendado utilizar, é melhor vc utilizar o operador identico para obter uma melhor resposta
+
+
+
+
+
