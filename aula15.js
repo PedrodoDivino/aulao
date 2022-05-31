9f72c5f592079d7553bb111388ec968b2c0cb457
Pedro.sandrini <pedro.sandri@markdigx.com.br>
2022-05-31 09:19:54 -0300

    [Pedro] Aula15 - Laços de repetição - FOR

---
+++ aula15.js
@@ -0,0 +1,15 @@
+// tipos de loops no javascript
+// 1 - for
+// 2 - while
+// 3 -  Do while
+// 4 - for  in 
+// 5 - for of 
+// operador modulos
+
+
+for (let index = 5; index >= 1 ; index--) {
+   if (1 / 2 !== 0){
+    console.log(index)
+   }
+
+}
